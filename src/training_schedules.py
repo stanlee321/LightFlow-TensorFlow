f4eb1e0b1ce72ab06f3c1f87ee7325186ed5155d
stanlee321 <stanlee321@gmail.com>
2018-12-06 23:33:51 -0400

    added code for retrain from scratch

--- src/training_schedules.py
+++ src/training_schedules.py
@@ -1,4 +1,13 @@
 LONG_SCHEDULE = {
+    'step_values': [10000, 30000, 40000, 50000, 60000 ],
+    'learning_rates': [0.001, 0.01, 0.01/2, (0.01/2)/2, ((0.01/2)/2)/2, (((0.01/2)/2)/2)/2],
+    'momentum': 0.9,
+    'momentum2': 0.999,
+    'weight_decay': 0.00004,
+    'max_iter':80000,
+}
+"""
+LONG_SCHEDULE = {
     'step_values': [190000, 210000, 220000, 230000, 240000 ],
     'learning_rates': [0.001, 0.01, 0.01/2, (0.01/2)/2, ((0.01/2)/2)/2, (((0.01/2)/2)/2)/2],
     'momentum': 0.9,
@@ -6,6 +15,7 @@ LONG_SCHEDULE = {
     'weight_decay': 0.00004,
     'max_iter':240000,
 }
+"""
 
 FINETUNE_SCHEDULE = {
     # TODO: Finetune schedule
