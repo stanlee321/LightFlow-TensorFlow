200cccd14b2d37ee682a05260f705ea0a986273d
stanlee321 <stanlee321@gmail.com>
2018-12-07 13:01:39 -0400

    updated learnign rate schdule

--- src/training_schedules.py
+++ src/training_schedules.py
@@ -1,10 +1,10 @@
 LONG_SCHEDULE = {
-    'step_values': [75000, 85000, 95000, 105000, 115000 ],
+    'step_values': [10000, 30000, 40000, 50000, 60000 ],
     'learning_rates': [0.001, 0.01, 0.01/2, (0.01/2)/2, ((0.01/2)/2)/2, (((0.01/2)/2)/2)/2],
     'momentum': 0.9,
     'momentum2': 0.999,
     'weight_decay': 0.00004,
-    'max_iter':140000,
+    'max_iter':100000,
 }
 """
 LONG_SCHEDULE = {
