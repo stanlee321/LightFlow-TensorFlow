b8bcacfffd6d7fd06f82c47e201e8993025b22c1
stanlee321 <stanlee321@gmail.com>
2018-12-12 16:34:27 -0400

    upated train and fixed model

--- src/lightflow/train.py
+++ src/lightflow/train.py
@@ -7,7 +7,7 @@ from .lightflow_tf import LightFlow
 net = LightFlow()
 
 # Load a batch of data
-input_a, input_b, flow = load_batch(FLYING_CHAIRS_DATASET_CONFIG, 'sample', net.global_step)
+input_a, input_b, flow = load_batch(FLYING_CHAIRS_DATASET_CONFIG, 'train', net.global_step)
 #
 # Train on the data
 net.train(
@@ -24,5 +24,5 @@ net.train(
     #checkpoints = {
     #    '../../logs/lightflow/model.ckpt-9468': ('lightflow/lightflow', 'lightflow')
     #}
-    checkpoints = 'latest'
+    checkpoints = False # 'latest'
 )
