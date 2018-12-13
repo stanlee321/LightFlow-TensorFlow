cb81ea82cac70d539622393fd11436dc9ca20a23
stanlee321 <stanlee321@gmail.com>
2018-12-12 23:40:57 -0400

    updated new lr based on 4 gpus training

--- src/training_schedules.py
+++ src/training_schedules.py
@@ -1,3 +1,4 @@
+"""
 LONG_SCHEDULE = {
     'step_values': [10000, 20000, 30000, 40000, 50000],
     'learning_rates': [0.001, 0.01, 0.01/2, (0.01/2)/2, ((0.01/2)/2)/2, (((0.01/2)/2)/2)/2],
@@ -5,6 +6,16 @@ LONG_SCHEDULE = {
     'momentum2': 0.999,
     'weight_decay': 0.00004,
     'max_iter': 75000,
+}
+"""
+
+LONG_SCHEDULE = {
+    'step_values': [40000, 80000, 120000, 16000, 200000],
+    'learning_rates': [0.001, 0.01, 0.01/2, (0.01/2)/2, ((0.01/2)/2)/2, (((0.01/2)/2)/2)/2],
+    'momentum': 0.9,
+    'momentum2': 0.999,
+    'weight_decay': 0.00004,
+    'max_iter': 300000,
 }
 
 FINETUNE_SCHEDULE = {
